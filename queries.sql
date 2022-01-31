0fe684d43a0a775c7b849ca49f6a47a4f7515d4d
MDaily7 <mdaily622@gmail.com>
2022-01-31 09:54:11 -0800

    updated queries.sql

--- queries.sql
+++ queries.sql
@@ -29,3 +29,28 @@ INTO retirement_info
 FROM Employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 And (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- create new table for retiring employees
+SELECT emp_no, first_name, last_name 
+INTO retirement_info
+FROM Employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+And (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+SELECT * FROM retirement_info;
+-- joining retirement_info and dept_emp tables
+SELECT  retirement_info.emp_no,
+		retirement_info.first_name,
+		retirement_info.last_name,
+		dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+-- left join retirement_info and dept_emp into a new table
+SELECT  ri.emp_no,
+		ri.first_name,
+		ri.last_name,
+		de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01)');
